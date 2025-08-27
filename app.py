b59ac091777bacd37a9d3cbbd4b6d36fc775d2d3
nqr_lty <nqr110@163.com>
2025-08-28 00:57:57 +0800

    底子基本OK

--- app.py
+++ app.py
@@ -14,6 +14,27 @@ def get_jinhua_geojson():
     try:
         with open('data/jinhua.geojson', 'r', encoding='utf-8') as f:
             geojson_data = json.load(f)
+        
+        # 地名映射表
+        name_mapping = {
+            "婺城区": "A地",
+            "金东区": "B地", 
+            "兰溪市": "C地",
+            "义乌市": "D地",
+            "东阳市": "E地",
+            "永康市": "F地",
+            "浦江县": "G地",
+            "武义县": "H地",
+            "磐安县": "I地"
+        }
+        
+        # 替换GeoJSON中的地名
+        for feature in geojson_data['features']:
+            if 'name' in feature['properties']:
+                original_name = feature['properties']['name']
+                if original_name in name_mapping:
+                    feature['properties']['name'] = name_mapping[original_name]
+        
         return jsonify(geojson_data)
     except FileNotFoundError:
         return jsonify({"error": "GeoJSON文件未找到"}), 404
@@ -25,15 +46,15 @@ def get_district_data():
     """提供各区县的模拟数据"""
     # 模拟金华市各区县的数据
     district_data = [
-        {"name": "婺城区", "value": 85, "population": 680000},
-        {"name": "金东区", "value": 72, "population": 350000},
-        {"name": "兰溪市", "value": 68, "population": 600000},
-        {"name": "义乌市", "value": 95, "population": 1200000},
-        {"name": "东阳市", "value": 78, "population": 850000},
-        {"name": "永康市", "value": 82, "population": 620000},
-        {"name": "浦江县", "value": 65, "population": 450000},
-        {"name": "武义县", "value": 60, "population": 320000},
-        {"name": "磐安县", "value": 55, "population": 220000}
+        {"name": "A地", "value": 85, "population": 680000},
+        {"name": "B地", "value": 72, "population": 350000},
+        {"name": "C地", "value": 68, "population": 600000},
+        {"name": "D地", "value": 95, "population": 1200000},
+        {"name": "E地", "value": 78, "population": 850000},
+        {"name": "F地", "value": 82, "population": 620000},
+        {"name": "G地", "value": 65, "population": 450000},
+        {"name": "H地", "value": 60, "population": 320000},
+        {"name": "I地", "value": 55, "population": 220000}
     ]
     return jsonify(district_data)
 
